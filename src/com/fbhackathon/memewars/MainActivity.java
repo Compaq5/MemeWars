aef8a96389d6d587fbf66d9ab2437a31747a7da2
Compaq5 <jan.medvesek@gmail.com>
2013-04-27 20:29:38 +0100

    Beacon

--- src/com/fbhackathon/memewars/MainActivity.java
+++ src/com/fbhackathon/memewars/MainActivity.java
@@ -1,10 +1,29 @@
 package com.fbhackathon.memewars;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 public class MainActivity extends Activity {
+	
+	private List<Bitmap> backgrounds = new ArrayList<Bitmap>();
+	
+	public View mainView;
+	public FrameLayout frameLayout;
+	
+	protected PowerManager.WakeLock mWakeLock;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +35,24 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
+		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // making it full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
+        // Set the hardware buttons to control the music
+        this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        
+        final PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        mWakeLock = pm.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, "My Tag"); // always On.
+		
+		mainView = new ViewIntroGame(this, backgrounds); 
+        frameLayout = new FrameLayout(this);
+        frameLayout.addView(mainView);
+        //View view = getAdvert();
+        //if(view != null) frameLayout.addView(view);
+        setContentView(frameLayout); 
+        
 		return true;
 	}
 
